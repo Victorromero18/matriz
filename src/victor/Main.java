cabae47cc67c1b58dfe4666efb62d5241dc56459
Victor Romero <vitymicky18@gmail.com>
2019-12-30 13:23:39 -0600

    Suma de las dos diagonales

--- src/victor/Main.java
+++ src/victor/Main.java
@@ -3,26 +3,35 @@ public class Main {
 	public static void main(String[]args) {
 		
 		int x[][];
-		x = new int[3][3];
+		x = new int[4][4];
 		x[0][0] = 1;
 		x[0][1] = 2;
 		x[0][2] = 3;
-		x[1][0] = 4;
-		x[1][1] = 5;
-		x[1][2] = 6;
-		x[2][0] = 7;
-		x[2][1] = 8;
-		x[2][2] = 9;
+		x[0][3] = 4;
+		x[1][0] = 5;
+		x[1][1] = 6;
+		x[1][2] = 7;
+		x[1][3] = 8;
+		x[2][0] = 9;
+		x[2][1] = 10;
+		x[2][2] = 11;
+		x[2][3] = 12;
+		x[3][0] = 13;
+		x[3][1] = 14;
+		x[3][2] = 15;
+		x[3][3] = 16;
 		
 		int diagonal=0;
-		
+		int diagonal2=0;
+		int e= x.length;
 		for(int i=0; i<x.length; i++) {
 			diagonal = diagonal + x[i][i];
-			
+			e--;
+			diagonal2 = diagonal2 + x[e][i];
 		}
 			
 		System.out.println(diagonal);
-		
+		System.out.println(diagonal2);
 	}
 	
 }
