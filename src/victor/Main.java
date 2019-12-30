11ceea046ac3d2518213e2b7b42463dcf65b4d22
Victor Romero <vitymicky18@gmail.com>
2019-12-30 12:51:57 -0600

    Suma de diagonal1 con for

--- src/victor/Main.java
+++ src/victor/Main.java
@@ -14,40 +14,17 @@ public class Main {
 		x[2][1] = 8;
 		x[2][2] = 9;
 		
-		int diagonal1 =0;
+		int diagonal=0;
 		
-		diagonal1 = x[0][0] + x[1][1] + x[2][2];
-		
-		System.out.println("La suma de la primer diagonal es: " + diagonal1);
-		
-		int diagonal2 =0;
-		
-		diagonal2 = x[2][0] + x[1][1] + x[0][2];
-		
-		System.out.println("La suma de la segunda diagonal es: " + diagonal2);
-		
-		int diagonal3 =0;
-		
-		diagonal3 = diagonal1 + diagonal2;
-		
-		System.out.println("La suma de las dos diagonales es: " + diagonal3);
-		
-		for(int i=0; i<3; i++) 
-		{
-			for(int j=0; j<3; j++) {
-				
-				System.out.print(x[i][j]);
-				
-			}
-			System.out.println();
+		for(int i=0; i<x.length; i++) {
+			diagonal = diagonal + x[i][i];
+			
 		}
-		
+			
+		System.out.println(diagonal);
 		
 	}
 	
 }
 
-//for (int i =0 ; i < 2; i ++)
-	//for (int j=0; j<2; j++)
-	//System.out.println(arr);
-//System.out.println("x["+i+"]["+j+"] = "+x[i][j]);
+
